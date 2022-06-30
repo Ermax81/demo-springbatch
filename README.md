793b40af5bc6513779a1e16408990dae1ac90a8c
Erwan N <enozay.dev@gmail.com>
2022-06-30 09:23:33 +0200

    Replacing hsqldb by mariadb

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-Source: https://spring.io/guides/gs/batch-processing/
+Sources: 
+- https://spring.io/guides/gs/batch-processing/
+- https://github.com/marco76/springBootBatch
+- https://springframework.guru/configuring-spring-boot-for-mariadb/
+- https://github.com/springframeworkguru/spring-boot-mariadb-example
+- https://www.rockyourcode.com/setup-mariadb-via-docker-with-spring-boot/
+- https://www.baeldung.com/spring-boot-data-sql-and-schema-sql
 
 JAVA11 SPRING_BATCH
 
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,20 @@
+drop table IF EXISTS `batch_job_execution_context`;
+drop table IF EXISTS `batch_job_execution_params`;
+drop table IF EXISTS `batch_job_execution_seq`;
+drop table IF EXISTS `batch_job_seq`;
+drop table IF EXISTS `batch_step_execution_context`;
+drop table IF EXISTS `batch_step_execution_seq`;
+drop table IF EXISTS `batch_step_execution`;
+drop table IF EXISTS `batch_job_execution`;
+drop table IF EXISTS `batch_job_instance`;
+
+drop table IF EXISTS `people`;
+
+CREATE TABLE `people`  (
+     person_id INT(11) NOT NULL AUTO_INCREMENT,
+     first_name VARCHAR(20),
+     last_name VARCHAR(20),
+     age INTEGER,
+     PRIMARY KEY (`person_id`)
+);
+
