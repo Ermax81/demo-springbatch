5d3cace906e1b1e346565563eb86b4a366cbad54
Erwan N <enozay.dev@gmail.com>
2022-07-08 01:25:48 +0200

    First example to test step sequence

--- src/main/java/com/example/batchprocessing/demo/BatchConfiguration.java
+++ src/main/java/com/example/batchprocessing/demo/BatchConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
@@ -82,22 +83,91 @@ public class BatchConfiguration {
 
     @Bean
     public Job importUserJob(JobCompletionNotificationListener listener, Step step1) {
+
+        // JOB_NAME in table BATCH_JOB_INSTANCE i
+        // JOB_EXECUTION_ID, START_TIME, END_TIME, STATUS... in table BATCH_JOB_EXECUTION e
+        // i.JOB_INSTANCE_ID = e.JOB_INSTANCE_ID
         return jobBuilderFactory.get("importUserJob")
                 .incrementer(new RunIdIncrementer()) //need an incrementer, because jobs use a database to maintain execution state
                 .listener(listener)
-                .flow(step1)
-                .end()
+
+                .start(step1)
+
+                .next(step2()).on("NOOP").end()
+
+                .from(step2()).on("COMPLETED").to(stepThink()).next(stepDone())
+
+                .from(step2()).on("FAILED").to(stepFail()).end()
+
                 .build();
     }
 
     @Bean
     public Step step1(JdbcBatchItemWriter<Person> writer) throws Exception {
+
+        // STEP_NAME, JOB_EXECUTION_ID, START_TIME, END_TIME, STATUS in table BATCH_STEP_EXECUTION
         return stepBuilderFactory.get("step1")
                 .<Person, Person> chunk(10)
                 .reader(reader())
                 .processor(processor())
                 //.processor(itemValidator())
                 .writer(writer)
+                .build();
+    }
+
+    @Bean
+    public Tasklet executeTasklet() {
+        String message = "2-Executing new step (step2)";
+        return new MessageTasklet(message, false);
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .allowStartIfComplete(true)
+                .tasklet(executeTasklet())
+                .build();
+    }
+
+    @Bean
+    public Tasklet failTasklet() {
+        String message = "Failing to build something...";
+        return new MessageTasklet(message, true);
+    }
+
+    @Bean
+    public Step stepFail() {
+        return stepBuilderFactory.get("stepFail")
+                .allowStartIfComplete(true)
+                .tasklet(failTasklet())
+                .build();
+    }
+
+    @Bean
+    public Tasklet doneTasklet() {
+        String message = "I'm done";
+        return new MessageTasklet(message, true);
+    }
+
+    @Bean
+    public Step stepDone() {
+        return stepBuilderFactory.get("stepDone")
+                .allowStartIfComplete(true)
+                .tasklet(doneTasklet())
+                .build();
+    }
+
+    @Bean
+    public Tasklet thinkTasklet() {
+        String message = "<Thinking of ...>";
+        return new MessageTasklet(message, true);
+    }
+
+    @Bean
+    public Step stepThink() {
+        return stepBuilderFactory.get("stepThink")
+                .allowStartIfComplete(true)
+                .tasklet(thinkTasklet())
                 .build();
     }
 
